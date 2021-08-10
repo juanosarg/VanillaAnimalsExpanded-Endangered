e07860fabb236e31b9206140ea887f6cb0ce6a69
juanosarg <31105173+juanosarg@users.noreply.github.com>
2021-08-10 09:31:40 +0200

    v1.104

--- 1.3/Source/VanillaAnimalsExpanded-Endangered/VanillaAnimalsExpandedEndangered/Harmony/Gizmo_Patch.cs
+++ 1.3/Source/VanillaAnimalsExpanded-Endangered/VanillaAnimalsExpandedEndangered/Harmony/Gizmo_Patch.cs
@@ -59,7 +59,7 @@ namespace VanillaAnimalsExpandedEndangered
 		};*/
 		public static void Postfix(ref IEnumerable<Gizmo> __result, Pawn __instance)
 		{
-			if (__instance.Faction == Faction.OfPlayer && (animalsToBanish.Contains(__instance.kindDef) || (VanillaAnimalsExpandedEndangeredMod.settings.addBanishToAllAnimals&& __instance.kindDef.RaceProps.Animal)))
+			if (__instance.Faction == Faction.OfPlayer && (animalsToBanish.Contains(__instance.kindDef) || (VanillaAnimalsExpandedEndangeredMod.settings.addBanishToAllAnimals&& __instance.kindDef.RaceProps.Animal&& !__instance.kindDef.RaceProps.FenceBlocked&& !__instance.Downed && !__instance.Dead)))
 			{
 				
 				List<Gizmo> list = __result.ToList<Gizmo>();
